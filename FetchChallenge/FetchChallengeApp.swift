4600ae3d623ff8f7ea73ab39953cd85314403024
akhilanandsirra <akhilanand9999@gmail.com>
2023-06-13 23:16:45 -0400

    Initial Commit

---
+++ FetchChallenge/FetchChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchChallengeApp.swift
+//  FetchChallenge
+//
+//  Created by Akhil Anand Sirra on 13/06/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
